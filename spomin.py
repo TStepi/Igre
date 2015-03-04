84ab9c3bef8e78be69a3723786707fe722e6c25d
TStepi <tomaz.stepi@gmail.com>
2015-03-04 20:33:48 +0100

    Dodano ogrodje za spomin.

---
+++ spomin.py
@@ -0,0 +1,58 @@
+from random import *
+
+n = 10
+k = 2
+
+
+
+
+
+def igra(k,n):
+    
+    def strategija1():
+        pogledani = set()
+        izbira = ()
+        for p in odkriti:
+            pogledani |= odkriti[p]
+            if len(odkriti[p]) == k:
+                izbira = tuple(odkriti[p])
+        if izbira != ():
+            return izbira
+        i = choice(list(preostali-pogledani))
+        prva = spomin[i]
+        if len(odkriti[prva]) == k-1:
+            return tuple([i]+list(odkriti[prva]))
+        
+            
+
+    def strategija2():
+        i = choice(list(preostali))
+        j = choice(list(preostali-{i}))
+        return (i,j)
+    
+    strat = [strategija1,strategija2]
+    spomin = k*list(range(n))
+    shuffle(spomin)
+    odkriti = {i:set() for i in range(n)}
+    preostali = set(range(2*n))
+    igralec = 0
+    prva = None
+    druga = None
+
+    najdeni = [0, 0]
+
+    while preostali:
+        f = strat[igralec]
+        (i,j) = f()
+        odkriti[spomin[i]].add(i)
+        odkriti[spomin[j]].add(j)
+        if spomin[i] == spomin[j]:
+            najdeni[igralec] += 1
+            preostali -= {i,j}
+        else:
+            igralec = 1 - igralec
+
+    return najdeni
+        
+    
+    
