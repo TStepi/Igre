2903519ffb938e1e3ea8bc6f6dcb511ba8bfc8b8
TStepi <tomaz.stepi@gmail.com>
2015-03-05 19:21:49 +0100

    Dodal še funkcijo za testiranje večih zaporednih iger. Dodal prvo verzijo svoje strategije. Zaenkrat samemu algoritmu za spomin toliko zaupam, da sem zakomentiral printe.

--- spomin.py
+++ spomin.py
@@ -6,24 +6,20 @@ n = 10
 k = 2
 
 def nakljucna(spomin, odkriti, neodkriti, mojipari, tvojipari, bednapoteza):
+    #print("Naključnež")
     mozni = {x for x in neodkriti}
     for x in odkriti.values():
         mozni |= x
-    print(mozni)
     i = choice(list(mozni))
     j = choice(list(mozni-{i}))
     return (i,j)
 
 
-#dvoboj = [taktikaMatej, taktikaTomaz]
-testT = [nakljucna, nakljucna]
-
-
 def igra(k,n,strat,prvi):
     spomin = k*list(range(n))
     shuffle(spomin)
-    print(spomin)
-    print()
+    #print(spomin)
+    #print()
     odkriti = {i:set() for i in range(n)}
     neodkriti = set(range(k*n))
     igralec = prvi
@@ -47,14 +43,26 @@ def igra(k,n,strat,prvi):
             if not (i in neodkriti or j in neodkriti):
                 prejsnjaNicNovih = True
         neodkriti -= {i,j}
-        print(i,j)
-        print(odkriti)
-        print(neodkriti)
-        print(najdeni)
-        print(predprejsnja,prejsnjaNicNovih)
-        print()
+        #print(i,j)
+        #print(odkriti)
+        #print(neodkriti)
+        #print(najdeni)
+        #print(predprejsnja,prejsnjaNicNovih)
+        #print()
         
     return najdeni
         
-    
-    
+def test(n,taktika1,taktika2,st):
+    """Naredi st simulacij igre spomina z n pari s strategijama taktika1 in taktika2, zacetni igralec se alternira. 
+        Prvo število v seznamu je število zmag taktike1, drugo je število izenačenj, tretje pa število zmag taktike 2. """
+    skupno = [0, 0, 0]
+    strat = [taktika1, taktika2]
+    for i in range(st):
+        rezultat = igra(2,n,strat,i%2)
+        if rezultat[0] > rezultat[1]:
+            skupno[0] += 1
+        elif rezultat[1] > rezultat[0]:
+            skupno[2] += 1
+        else:
+            skupno[1] += 1
+    print(skupno)
--- tomaz.py
+++ tomaz.py
@@ -1,17 +1,22 @@
+from random import choice
+
 def taktikaTomaz(spomin, odkriti, neodkriti, mojipari, tvojipari, safety):
-    pass
-    
-    
-    def strategija1():
-        pogledani = set()
-        izbira = ()
-        for p in odkriti:
-            pogledani |= odkriti[p]
-            if len(odkriti[p]) == k:
-                izbira = tuple(odkriti[p])
-        if izbira != ():
-            return izbira
-        i = choice(list(preostali-pogledani))
-        prva = spomin[i]
-        if len(odkriti[prva]) == k-1:
-            return tuple([i]+list(odkriti[prva]))
+    #print("Tomaž")
+    pogledani = set()
+    izbira = ()
+    for p in odkriti:
+        pogledani |= odkriti[p]
+        if len(odkriti[p]) == 2:
+            izbira = tuple(odkriti[p])
+    if izbira != ():
+        return izbira
+        
+    i = choice(list(neodkriti))
+    prva = spomin[i]
+    if len(odkriti[prva]) == 1:
+        return tuple([i]+list(odkriti[prva]))
+        
+    elif pogledani:
+        return (i,choice(list(pogledani)))
+    else:
+        return (i,choice(list(neodkriti-{i})))
